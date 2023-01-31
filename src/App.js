4f9131a1553c8b0379ea1767983329bd3cf38f50
Israel Kenneth <42800016+IsraelKenneth@users.noreply.github.com>
2023-01-31 15:47:29 +0100

    Correction

--- src/App.js
+++ src/App.js
@@ -1,65 +1,25 @@
-import React from 'react'
-import styles from './style'
-import "./App.css"
-import { Introducing, CTA, Navbar, Stats, Footer, Hero, Jamila, Education, Stats2, Gallery, Packages, CTA2, FAQ, Testimonials } from './components'
+import React from "react";
+import { Route, Router, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import About from "./routes/About";
+import Contact from "./routes/Contact";
+import Home from "./routes/Home";
+import Service from "./routes/Service";
 
-const App = () => {
+function App() {
   return (
-    <div className="bg-overal w-full overflow-hidden ">
-          <div className={`${styles.paddingX} ${styles.flexCenter} `} >
-          <div className={`${styles.boxWidth}`}>
-        <Navbar />
-          </div>
-          </div>
-      <div className={`bg-[#F9F9F9] ${styles.flexStart}`}>
-          <div className={`${styles.boxWidth}`}>
-        <Hero />
-          </div>
-          </div>
-
-        <div className={`bg-[#FFFFFF] ${styles.paddingX} ${styles.flexStart}`}>
-          <div className={`${styles.boxWidth}`}>
-            <Stats />
-            <Introducing />
-            <Jamila /> 
-            <Education />
-        </div>
-      </div>
-          <div className={`bg-image-Stats ss:my-[4rem] my-[1rem]  ${styles.flexStart}`}>
-            <div className={`${styles.boxWidth}`}>
-              <Stats2 />
-            </div>
-          </div>
-
-      <div className={`bg-[#FFFFFF] ${styles.paddingX} ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
-          <Gallery />
-          <CTA />
-          <Packages />
-        </div>
-      </div>
-          <div className={`bg-Footer-CTA mt-10 mb-10 ${styles.flexStart}`}>
-            <div className={`${styles.boxWidth}`}>
-              <CTA2/>
-            </div>
-          </div>
-      <div className={`bg-[#FFFFFF] ${styles.paddingX} ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
-          <FAQ />
-        </div>
-      </div>
-          <div className={`mt-10 mb-10 ${styles.flexStart}`}>
-            <div className={`${styles.boxWidth}`}>
-            <Testimonials />
-            <Footer />
-            </div>
-          </div>
-
-          
-
-      </div>
-
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      {/* <Navbar /> */}
+      <Footer />
+    </div>
   )
 }
 
-export default App
+export default App;
